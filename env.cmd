c548720fa074102b2ec88f725ecaed14b11d1e2d
dsparrow <dsparrow27@gmail.com>
2020-11-07 17:22:23 +1100

    localizing zoo package versions

--- env.cmd
+++ env.cmd
@@ -1,7 +1,7 @@
 for %%i in ("%~dp0.") do SET "VORTEX=%%~fi"
 set DEBUG=1
 set ZOOTOOLS_ROOT=F:\code\python\personal\zootoolspro
-set ZOO_PACKAGE_VERSION_PATH=%VORTEX%\package_version.config
+set ZOO_PACKAGE_VERSION_PATH=%VORTEX%\zoo\package_version.config
 set PATH=%PATH%;%VORTEX%;%ZOOTOOLS_ROOT%\install\core\python
 set VORTEX_ICONS=%VORTEX%\icons
 set PYTHONPATH=%PYTHONPATH%;%VORTEX%;%ZOOTOOLS_ROOT%\install\core\python
